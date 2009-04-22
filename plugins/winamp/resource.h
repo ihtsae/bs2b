de9d57514f2361207ee0cb28018aa38715c703ac
boris_mikhaylov <boris_mikhaylov@bc0edfbe-c936-4687-b64d-f70bc3985e72>
2009-04-22 11:23:27 +0000

    winamp plugin 3.0.0 released.

---
+++ plugins/winamp/resource.h
@@ -0,0 +1,12 @@
+#define IDD_CONFIG_BS2B             101
+#ifndef IDC_STATIC
+#define IDC_STATIC                  -1
+#endif
+#define IDC_SLIDER_LEVEL_FEED       1001
+#define IDC_SLIDER_LEVEL_FCUT       1002
+#define IDC_BUTTON_ABOUT            1003
+#define IDC_BUTTON_DEFAULT          1004
+#define IDC_BUTTON_CMOY             1005
+#define IDC_BUTTON_JMEIER           1006
+#define IDC_STATIC_LEVEL_FEED       1007
+#define IDC_STATIC_LEVEL_FCUT       1008
